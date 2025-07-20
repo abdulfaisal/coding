8f5feaca4ef882183ecf0839963ada6cee5b3d46
abdulfaisal <abdulfaisalup@gmail.com>
2025-07-20 14:39:33 +0530

    finish course

---
+++ DSAPrep/src/main/java/org/example/graphs/FinishCOursesPrereq.java
@@ -0,0 +1,50 @@
+package org.example.graphs;
+
+import org.example.binarysearch.SearchInSortedArray;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FinishCOursesPrereq {
+        int[][] graph;
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> path = new HashSet<>();
+
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            graph = new int[numCourses][numCourses];
+
+            // Build adjacency matrix
+            for (int[] pre : prerequisites) {
+                int to = pre[0];
+                int from = pre[1];
+                graph[from][to] = 1;
+            }
+
+            // DFS for each unvisited node
+            for (int i = 0; i < numCourses; i++) {
+                if (!visited.contains(i)) {
+                    if (dfs(i)) return false; // Cycle detected
+                }
+            }
+
+            return true; // No cycle found
+        }
+
+        private boolean dfs(int node) {
+            visited.add(node);
+            path.add(node); // Add to current DFS path
+
+            for (int neighbor = 0; neighbor < graph.length; neighbor++) {
+                if (graph[node][neighbor] == 1) {
+                    if (!visited.contains(neighbor)) {
+                        if (dfs(neighbor)) return true;
+                    } else if (path.contains(neighbor)) {
+                        return true; // Back edge → cycle
+                    }
+                }
+            }
+
+            path.remove(node); // Backtrack
+            return false;
+        }
+}
--- DSAPrep/src/main/java/org/example/graphs/Main.java
+++ DSAPrep/src/main/java/org/example/graphs/Main.java
@@ -3,6 +3,27 @@ package org.example.graphs;
 public class Main {
 
     public static void main(String[] args) {
+//        numberOfislands;
+
+        FinishCOursesPrereq finishCOursesPrereq = new FinishCOursesPrereq();
+//
+        int prereqArr[][] = {{0,1},{1,0}};
+        System.out.println(finishCOursesPrereq.canFinish(2,prereqArr));
+
+        int prereqArr2[][] = {{0, 1}};
+        System.out.println(finishCOursesPrereq.canFinish(2,prereqArr2));
+
+        int prerequisites[][] ={{1,4},{2,4},{3,1},{3,2}};
+        System.out.println(finishCOursesPrereq.canFinish(5,prerequisites));
+
+//
+
+        int prerequisites2[][] ={{0,1},{0,2},{1,2}};
+        System.out.println(finishCOursesPrereq.canFinish(5,prerequisites2));
+
+    }
+
+    static void numberOfislands() {
         char[][] grid = {
                 {'1', '1', '1', '1', '0'},
                 {'0', '1', '0', '1', '0'},
